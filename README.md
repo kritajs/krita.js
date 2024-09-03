3efc16dfdaddce4f29f3cf844830cac79c752c93
Tommy <51040159+tommyquant@users.noreply.github.com>
2024-09-03 12:07:00 +1000

    Get Sciter working

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # krita.js
 
 krita.js is a Krita plugin that allows users to write scripts using JavaScript.
+
+## Getting started
+
+1. Download the [Sciter SDK](https://sciter.com/download/).
+2. Create a `bin` folder in this repo and copy `sciter-js-sdk-main/bin/windows/x64/sciter.dll` into it.
+3. Run `python ./src/main.py` to start the example project.
--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 # Add lib folder to sys.path so we can import packages from lib
 p = Path(__file__).parent.parent / "lib"
-sys.path.append(p.absolute().as_posix())
+sys.path.append(str(p))
 
 import sciter
 
 if __name__ == '__main__':
     frame = sciter.Window(ismain=True, uni_theme=True)
-    frame.load_file("minimal.htm")
+    frame.load_file(str(Path(__file__).parent / "minimal.htm"))
     frame.expand()
     frame.run_app()
