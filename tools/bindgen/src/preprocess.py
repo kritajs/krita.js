04f9c8b93f57db161608aac947c0dfd12cf052e7
tommyquant <51040159+tommyquant@users.noreply.github.com>
2024-09-12 13:02:01 +1000

    Generate C++ headers (#6)

--- tools/bindgen/src/preprocess.py
+++ tools/bindgen/src/preprocess.py
@@ -1,12 +1,6 @@
-import argparse
-import dataclasses
-import glob
 import io
-import json
 import re
 import typing
-from cxxheaderparser.simple import parse_string
-from pathlib import Path
 from pcpp import Preprocessor, OutputDirective, Action
 
 # Custom preprocessor for pcpp
@@ -26,9 +20,9 @@ class CustomPreprocessor(Preprocessor):
     def parse(self, input, source=None):
         super().parse(input, source)
         if self.errors:
-            raise Exception("\n".join(pp.errors))
+            raise Exception("\n".join(self.errors))
         elif self.return_code:
-            raise Exception("Failed with exit code %d" % pp.return_code)
+            raise Exception("Failed with exit code %d" % self.return_code)
         fp = io.StringIO()
         self.write(fp)
         fp.seek(0)
@@ -61,32 +55,7 @@ class CustomPreprocessor(Preprocessor):
     def on_comment(self, tok):
         return True
 
-# Script requires caller to pass in path to the `libkis` directory
-parser = argparse.ArgumentParser()
-parser.add_argument("libkis_dir")
-args = parser.parse_args()
-libkis_dir = Path(args.libkis_dir)
-print("libkis directory: " + str(libkis_dir))
-
-# Parse all libkis headers
-files = glob.glob("*.h", root_dir=str(libkis_dir))
-for file_name in files:
-    print("Processing " + file_name + "...")
-    file_path = libkis_dir / file_name
-
-    # Preprocess to remove directives, macros etc.
+def preprocess(file_name: str, source_code: str) -> str:
     pp = CustomPreprocessor()
-    preprocessed = pp.parse(file_path.read_text(), file_name)
-    # Write preprocessed header to a file
-    preprocessed_file = Path(__file__).parent / "generated" / file_name
-    preprocessed_file.parent.mkdir(exist_ok=True, parents=True)
-    preprocessed_file.write_text(preprocessed)
-
-    # Parse preprocessed output
-    parsed_data = parse_string(preprocessed)
-    ddata = dataclasses.asdict(parsed_data)
-    json_data = json.dumps(ddata, indent=4)
-    # Write parser output to a JSON file
-    parsed_file = Path(__file__).parent / "generated" / (file_name + ".json")
-    parsed_file.parent.mkdir(exist_ok=True, parents=True)
-    parsed_file.write_text(json_data)
+    preprocessed = pp.parse(source_code, file_name)
+    return preprocessed
