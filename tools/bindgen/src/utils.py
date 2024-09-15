03b76e84cd8a35b0a6b99b4df77a7285f69c4072
Vincent Sit <vincentsit00@ymail.com>
2024-09-15 15:20:03 +1200

    Remove unused function

--- tools/bindgen/src/utils.py
+++ tools/bindgen/src/utils.py
@@ -3,6 +3,3 @@ from pathlib import Path
 def write_file(path: Path, content: str):
     path.parent.mkdir(exist_ok=True, parents=True)
     path.write_text(content)
-
-def delete_file(path: Path):
-    path.unlink()
