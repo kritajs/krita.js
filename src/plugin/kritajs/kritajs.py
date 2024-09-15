f8efece7931ad1c29008d4408f2a33d8b0b56ccd
tommyquant <51040159+tommyquant@users.noreply.github.com>
2024-09-15 11:15:20 +1000

    Implement Ultralight prototype (#11)

--- src/plugin/kritajs/kritajs.py
+++ src/plugin/kritajs/kritajs.py
@@ -1,5 +1,6 @@
 from krita import *
 import ctypes
+import traceback
 from pathlib import Path
 from PyQt5.QtCore import qDebug
 
@@ -20,13 +21,22 @@ def start():
     qwindow = Krita.instance().activeWindow().qwindow()
     qwindow.destroyed.connect(dispose)
 
-    lib_path = Path(__file__).parent / "bin/loader.dll"
-    lib = ctypes.CDLL(str(lib_path))
-    init_kritajs = lib.KRITAJS
-    init_kritajs.argtypes = [ctypes.c_int]
-    value = 5
-    results = init_kritajs(value)
-    qDebug(str(results))
+    try:
+        # Load Ultralight and krita.js libraries
+        base_path = Path(__file__).parent
+        bin_path = base_path / "bin"
+        ctypes.CDLL(str(bin_path / "AppCore"))
+        ctypes.CDLL(str(bin_path / "Ultralight"))
+        ctypes.CDLL(str(bin_path / "UltralightCore"))
+        ctypes.CDLL(str(bin_path / "WebCore"))
+        kritajs_lib = ctypes.CDLL(str(bin_path / "kritajs"))
+
+        # Initialize krita.js
+        init_kritajs = kritajs_lib.KRITAJS
+        init_kritajs.argtypes = [ctypes.c_char_p]
+        init_kritajs(str(base_path).encode("utf-8"))
+    except OSError as e:
+        qDebug(traceback.format_exc())
 
 def dispose():
     pass
